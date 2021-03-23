e012f6f5bb3faf230e5600e28968b8c40b88ca05
ilyak93 <ilyak93@gmail.com>
2021-03-24 01:46:42 +0200

    ImageTransformer Global Attention

--- SinGAN/models.py
+++ SinGAN/models.py
@@ -394,7 +394,7 @@ class ImageAttn(nn.Module):
         result = result.permute([0, 2, 1, 3]).contiguous()
         result = result.view(result.shape[0:2] + (-1,))
         result = self.output_dense(result)
-        result = result.view(orig_shape[0], orig_shape[1], orig_shape[2], orig_shape[3]).permute([0, 3, 1, 2])
+        result = result.view(orig_shape[0], orig_shape[1], orig_shape[2], orig_shape[3])  # .permute([0, 3, 1, 2])
         return result
 
 
@@ -409,16 +409,20 @@ class ImgAttnWDiscriminator(nn.Module):
             N = int(opt.nfc / pow(2, (i + 1)))
             block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
             self.body.add_module('block%d' % (i + 1), block)
-        if opt.attn == True:
-            self.attn = ImageAttn(in_dim=max(N, opt.min_nfc), num_heads=4, block_length=-1)
-            self.gamma = nn.Parameter(torch.zeros(1))
         self.tail = nn.Conv2d(max(N, opt.min_nfc), 1, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size)
+        if opt.attn == True:
+            self.attn = ImageAttn(
+                in_dim=max(N, opt.min_nfc),
+                num_heads=4,
+                block_length=max(N, opt.min_nfc),
+            )
+            self.gamma = nn.Parameter(torch.zeros(1))
 
     def forward(self, x):
         x = self.head(x)
         x = self.body(x)
         if hasattr(self, 'attn'):
-            x = self.gamma * self.attn(x) + x
+            x = self.gamma * x + self.attn(x).permute([0, 3, 1, 2])
         x = self.tail(x)
         return x
 
@@ -435,108 +439,26 @@ class ImageAttnGeneratorConcatSkip2CleanAdd(nn.Module):
             N = int(opt.nfc / pow(2, (i + 1)))
             block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
             self.body.add_module('block%d' % (i + 1), block)
-        if opt.attn == True:
-            self.attn = ImageAttn(in_dim=max(N, opt.min_nfc), num_heads=4, block_length=-1)
-            self.gamma = nn.Parameter(torch.zeros(1))
+
         self.tail = nn.Sequential(
             nn.Conv2d(max(N, opt.min_nfc), opt.nc_im, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size),
             nn.Tanh()
         )
+        if opt.attn == True:
+            self.attn = ImageAttn(
+                in_dim=max(N, opt.min_nfc),
+                num_heads=4,
+                block_length=max(N, opt.min_nfc),
+            )
+            self.gamma = nn.Parameter(torch.zeros(1))
 
     def forward(self, x, y):
         x = self.head(x)
         x = self.body(x)
         if hasattr(self, 'attn'):
-            x = self.gamma * self.attn(x) + x
+            x = self.gamma * x + self.attn(x).permute([0, 3, 1, 2])
         x = self.tail(x)
         ind = int((y.shape[2] - x.shape[2]) / 2)
         y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]
         return x + y
-        
-        
-class DecoderAttnLayer(nn.Module):
-    """Implements a single layer of an unconditional ImageTransformer"""
-    def __init__(self, in_dim, num_heads, block_length, dropout=0.1):
-        super().__init__()
-        self.attn = ImageAttn(in_dim, num_heads, block_length)
-        self.dropout = nn.Dropout(p=dropout)
-        self.layernorm_attn = nn.LayerNorm([in_dim], eps=1e-6, elementwise_affine=True)
-        self.layernorm_ffn = nn.LayerNorm([in_dim], eps=1e-6, elementwise_affine=True)
-        self.ffn = nn.Sequential(nn.Linear(in_dim, 2*in_dim, bias=True),
-                                 nn.ReLU(),
-                                 nn.Linear(2*in_dim, in_dim, bias=True))
-
-
-    # Takes care of the "postprocessing" from tensorflow code with the layernorm and dropout
-    def forward(self, X):
-        y = self.attn(X)
-        X = X.permute([0, 2, 3, 1])
-        X = self.layernorm_attn(self.dropout(y) + X)
-        y = self.ffn(X)
-        X = self.layernorm_ffn(self.dropout(y) + X)
-        return X.permute([0, 3, 1, 2]).contiguous()
-
-#exact image transformer implementation
-class ImgTransfromerWDiscriminator(nn.Module):
-    def __init__(self, opt):
-        super(ImgTransfromerWDiscriminator, self).__init__()
-        self.is_cuda = torch.cuda.is_available()
-        N = int(opt.nfc)
-        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size, 1)
-        self.body = nn.Sequential()
-        for i in range(opt.num_layer - 2):
-            N = int(opt.nfc / pow(2, (i + 1)))
-            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
-            self.body.add_module('block%d' % (i + 1), block)
-        self.tail = nn.Conv2d(max(N, opt.min_nfc), 1, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size)
-        if opt.attn == True:
-            self.attn = DecoderAttnLayer(
-                in_dim = max(N, opt.min_nfc),
-                num_heads = 4,
-                block_length = max(N, opt.min_nfc),
-            )
-
-    def forward(self, x):
-        x = self.head(x)
-        x = self.body(x)
-        if hasattr(self,'attn'):
-            x = self.attn(x)
-        x = self.tail(x)
-        return x
-
-
-class ImgTransfromerGeneratorConcatSkip2CleanAdd(nn.Module):
-    def __init__(self, opt):
-        super(ImgTransfromerGeneratorConcatSkip2CleanAdd, self).__init__()
-        self.is_cuda = torch.cuda.is_available()
-        N = opt.nfc
-        self.head = ConvBlock(opt.nc_im, N, opt.ker_size, opt.padd_size,
-                              1)  # GenConvTransBlock(opt.nc_z,N,opt.ker_size,opt.padd_size,opt.stride)
-        self.body = nn.Sequential()
-        for i in range(opt.num_layer - 2):
-            N = int(opt.nfc / pow(2, (i + 1)))
-            block = ConvBlock(max(2 * N, opt.min_nfc), max(N, opt.min_nfc), opt.ker_size, opt.padd_size, 1)
-            self.body.add_module('block%d' % (i + 1), block)
-
-        self.tail = nn.Sequential(
-            nn.Conv2d(max(N, opt.min_nfc), opt.nc_im, kernel_size=opt.ker_size, stride=1, padding=opt.padd_size),
-            nn.Tanh()
-        )
-        if opt.attn == True:
-            self.attn = DecoderAttnLayer(
-                in_dim=max(N, opt.min_nfc),
-                num_heads=4,
-                block_length=max(N, opt.min_nfc),
-            )
-
-    def forward(self, x, y):
-        x = self.head(x)
-        x = self.body(x)
-        if hasattr(self,'attn'):
-            x = self.attn(x)
-        x = self.tail(x)
-        ind = int((y.shape[2] - x.shape[2]) / 2)
-        y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]
-        return x + y
-
 
