20f02529be7f57a19a739f8dc50784e05980be8a
ilyak93 <ilyak93@gmail.com>
2021-03-24 07:34:19 +0200

    Update training.py

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -30,7 +30,9 @@ def train(opt,Gs,Zs,reals,NoiseAmp):
         #plt.imsave('%s/in.png' %  (opt.out_), functions.convert_image_np(real), vmin=0, vmax=1)
         #plt.imsave('%s/original.png' %  (opt.out_), functions.convert_image_np(real_), vmin=0, vmax=1)
         plt.imsave('%s/real_scale.png' %  (opt.outf), functions.convert_image_np(reals[scale_num]), vmin=0, vmax=1)
-
+        
+        _, _, h, w = reals[scale_num].size()
+        opt.image_size = h-8, w-8
         D_curr,G_curr = init_models(opt)
         if (nfc_prev==opt.nfc):
             G_curr.load_state_dict(torch.load('%s/%d/netG.pth' % (opt.out_,scale_num-1)))
